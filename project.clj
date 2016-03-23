ac5001c448ebce9554d6f510b26e21b46bc5f459
Anonymous Candidate <anon@example.com>
2016-03-23 11:05:34 -0700

    Add sufficiently retro 'under construction' page
    This sorts out configuring some of the ring middleware we
    need.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  [compojure "1.5.0"]
                  [kosmos "0.0.4"]
                  [kosmos/kosmos-web "0.0.2"]
+                 [hiccup "1.0.5"]
                  [kosmos/kosmos-hsqldb-server "0.0.1"]]
   :main ^:skip-aot psdm.core
   :target-path "target/%s"
--- src/psdm/server.clj
+++ src/psdm/server.clj
@@ -1,6 +1,28 @@
-(ns psdm.server)
+(ns psdm.server
+  (:require [hiccup.core :refer :all]
+            [ring.middleware.content-type :as content-type]
+            [ring.middleware.resource :as resource]))
 
-(defn app [request]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "You did it!"})
+(defn handler [_]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (html [:head [:title "Productivity Self Delusion Machine"]]
+                  [:body
+                   [:h1 "Productivity Self Delusion Machine"]
+                   [:h2 "For when actual productivity is just too hard"]
+                   [:table
+                    [:tr
+                     [:td
+                      [:img {:src "/assets/images/animated-under-construction.gif"}]]
+                     [:td
+                      [:marquee "!!Under Construction!! Weren't the 90s Amazing!"]]
+                     [:td
+                      [:img {:src "/assets/images/animated-under-construction.gif"}]]]
+                    [:tr
+                     [:td]
+                     [:td]
+                     [:td "Best viewed with:" [:br] [:img {:src "/assets/images/netnow3.gif"}]]]]])})
+
+(def app (-> handler
+             (resource/wrap-resource "public")
+             content-type/wrap-content-type))
