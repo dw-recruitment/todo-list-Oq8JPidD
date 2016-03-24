656b8608cc91d3216f6f20a28cff77cf0ef16de4
Anonymous Candidate <anon@example.com>
2016-03-23 21:40:08 -0700

    Abstract out layout
    Pulled basic page structure into a sperate function. Not that there
    really is much to structure at this juncture.

--- src/psdm/server.clj
+++ src/psdm/server.clj
@@ -1,5 +1,6 @@
 (ns psdm.server
   (:require [hiccup.core :refer :all]
+            [hiccup.page :refer [html5]]
             [psdm.config :as config]
             [compojure.core :refer [routes GET]]
             [compojure.route :as route]
@@ -7,29 +8,35 @@
             [ring.middleware.resource :as resource]
             [ring.middleware.reload :as reload]))
 
-(defn index [_]
-  (html [:head [:title "Productivity Self Delusion Machine"]]
-        [:body
-         [:h1 "Productivity Self Delusion Machine"]
-         [:h2 "For when actual productivity is just too hard"]
-         [:table
-          [:tr
-           [:td
-            [:img {:src "/assets/images/animated-under-construction.gif"}]]
-           [:td
-            [:marquee "!!Under Construction!! Weren't the 90s Amazing!"]]
-           [:td
-            [:img {:src "/assets/images/animated-under-construction.gif"}]]]
-          [:tr
-           [:td]
-           [:td]
-           [:td "Best viewed with:" [:br] [:img {:src "/assets/images/netnow3.gif"}]]]]]))
+(defn layout [req page-name & contents]
+  (html5
+    {:lang "en"}
+    [:head [:title "Productivity Self Delusion Machine"]]
+    [:body
+     [:h1 page-name]
+     contents]))
 
-(defn about [_]
-  "About things!")
+(defn index [req]
+  (layout req "Productivity Self Delusion Machine"
+          [:h2 "For when actual productivity is just too hard"]
+          [:table
+           [:tr
+            [:td
+             [:img {:src "/assets/images/animated-under-construction.gif"}]]
+            [:td
+             [:marquee "!!Under Construction!! Weren't the 90s Amazing!"]]
+            [:td
+             [:img {:src "/assets/images/animated-under-construction.gif"}]]]
+           [:tr
+            [:td]
+            [:td]
+            [:td "Best viewed with:" [:br] [:img {:src "/assets/images/netnow3.gif"}]]]]))
 
-(defn not-found [_]
-  "Not Found!")
+(defn about [req]
+  (layout req "About"))
+
+(defn not-found [req]
+  (layout req "Not Found!"))
 
 (def handler
   (routes
