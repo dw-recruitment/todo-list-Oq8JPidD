fd6f07db95b804ee4af711a1199cfc486fa89bcb
Anonymous Candidate <anon@example.com>
2016-03-26 12:21:25 -0700

    Add logback and address java logging nightmare
    Exceptions were happening on certain requests but nothing was being
    logged, hence it is time to get the logging unbusted. There are two
    popular log handlers in Javaland: log4j and logback. Both are made by
    the same person, and you should use logback now (without getting into
    the details, it's the author's recommendation).
    
    To make matters more complicated, there are also two logging APIs in
    Java: slf4j and commons-logging. Logback requires slf4j, but a lot of
    libraries still use commons-logging. The lazy thing to do here is just
    use and adapter so that log4j can be used with slf4j. This will work,
    but that's living in the past. Logback, for a lot of reasons, is a
    significant improvement to log4j.
    
    The way we fix this problem is by including jcl-over-slf4j which is a
    reimplementation of commons-logging that uses slf4j. Now our
    dependencies that rely commons-logging can use slf4j. Next, we include
    an empty commons-logging artifact to ensure commons-logging doesn't
    get on the classpath and cause mischief. Then we can include
    logback-classic. See, not so bad really.

--- project.clj
+++ project.clj
@@ -9,9 +9,31 @@
                  [kosmos/kosmos-hikari "0.0.1"]
                  [ring/ring-devel "1.4.0"]
                  [honeysql "0.6.3"]
-                 [ragtime "0.5.3"]]
+                 [ragtime "0.5.3"]
+                 ;; the logging API
+                 [org.slf4j/slf4j-api "1.7.19"]
+                 ;; this is an empty commons-logging artifact that will replace
+                 ;; the version of this library coming from any of our
+                 ;; dependencies. We could just exclude the artifact from each
+                 ;; our dependencies, but someone will forget to do that, and
+                 ;; only after many hours of painful debugging will we discover
+                 ;; what happened. This way is less brittle.
+                 [commons-logging/commons-logging "99-empty"]
+                 ;; this is a reimplementation of commons-logging that uses
+                 ;; slf4j. Basically, it allows us to use slf4j for libraries
+                 ;; that were written for commons-logging.
+                 [org.slf4j/jcl-over-slf4j "1.7.19"]
+                 ;; this is to get java util logging to use slf4j, not that
+                 ;; anyone actually uses that
+                 [org.slf4j/jul-to-slf4j "1.7.19"]
+                 ;; at long last, logback, a logging provider for slf4j
+                 [ch.qos.logback/logback-classic "1.1.6"]
+                 ]
   :main ^:skip-aot psdm.core
   :target-path "target/%s"
   :aliases {"db" ["run" "-m" "psdm.migration"]}
+  ;; The version99 repo we need for an empty commons-logging artifact. This will
+  ;; help us deal with the nightmare that is logging in java.
+  :repositories [["version99" "http://version99.qos.ch/"]]
   :profiles {:uberjar {:aot :all}
              :dev     {:dependencies [[ring/ring-mock "0.3.0"]]}})
